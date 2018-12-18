cb04b2427d698c8dec454a86ec3afcca9ab8224a
Adrian Hall <adrian@shellmonger.com>
2018-12-18 13:43:09 -0800

    Processing of me query

--- mobile-app/src/screens/user/UserBlock.tsx
+++ mobile-app/src/screens/user/UserBlock.tsx
@@ -1,9 +1,10 @@
 import { Ionicons } from '@expo/vector-icons';
-import { graphqlOperation } from 'aws-amplify';
+import { API, graphqlOperation } from 'aws-amplify';
 import { Connect } from 'aws-amplify-react-native';
 import * as React from 'react';
-import { ActivityIndicator, StyleSheet, Text, View, ViewStyle, TextStyle } from 'react-native';
+import { ActivityIndicator, StyleSheet, TextInput, TextStyle, View, ViewStyle } from 'react-native';
 import Colors from '../../constants/Colors';
+import { ConsoleLogger } from '@aws-amplify/core';
 
 const styles = StyleSheet.create({
     innerProfile: {
@@ -55,9 +56,18 @@ interface IUserBlockProps {
     loading?: boolean;
     errors?: string[];
     name?: string | null;
+    updateName?: (name: string) => Promise<boolean>;
 }
 
-class UserBlock extends React.Component<IUserBlockProps, {}> {
+interface IUserBlockState {
+    name: string;
+}
+
+class UserBlock extends React.Component<IUserBlockProps, IUserBlockState> {
+    public state: IUserBlockState = {
+        name: this.props.name || ''
+    };
+
     public render() {
         if (this.props.loading) {
             return (
@@ -70,7 +80,7 @@ class UserBlock extends React.Component<IUserBlockProps, {}> {
         if (this.props.errors && this.props.errors.length > 0) {
             return (
                 <View style={styles.outerCentered}>
-                    <Ionicons name="bug" size={48} color={Colors.palette.tertiary[2]}/>
+                    <Ionicons name="md-bug" size={48} color={Colors.palette.tertiary[2]}/>
                 </View>
             );
         }
@@ -81,13 +91,44 @@ class UserBlock extends React.Component<IUserBlockProps, {}> {
                 </View>
                 <View style={styles.innerTitle}>
                     <View style={styles.nameBlock}>
-                        <Text style={styles.name}>{this.props.name || 'Unknown'}</Text>
+                        <TextInput
+                            autoCapitalize="words"
+                            autoCorrect={false}
+                            autoFocus={false}
+                            blurOnSubmit={true}
+                            clearButtonMode="while-editing"
+                            keyboardType="default"
+                            keyboardAppearance="light"
+                            maxLength={48}
+                            onChangeText={(text) => this.setState({ name: text })}
+                            onEndEditing={() => this.submitName()}
+                            placeholder="Your Name Here"
+                            placeholderTextColor={Colors.palette.secondary[4]}
+                            returnKeyType="done"
+                            style={styles.name}
+                            textContentType="name"
+                            underlineColorAndroid={Colors.palette.secondary[4]}
+                            value={this.state.name}
+                        />
                     </View>
                     <View style={styles.statsBlock}>
                     </View>
                 </View>
             </View>
         );
+    }
+
+    private async submitName() {
+        if (!this.props.updateName) return;
+
+        // TODO: Error handling for submission
+        const result = await this.props.updateName(this.state.name);
+        if (!result) {
+            // Do a pop-up to alert that the submission failed
+
+            // Then set the state back to the original
+            this.setState({ name: this.props.name || '' });
+        }
     }
 }
 
@@ -103,21 +144,27 @@ interface IUserBlockQueryResponse {
 }
 
 const UserBlockFunction = () => {
-    const query = `
-    {
-        me {
-            id
-            name
+    const query = `{
+        me { id name }
+    }`;
+
+    const updateNameMutation = async(name: string): Promise<boolean> => {
+        try {
+            const mutation = 'mutation UpdateUser($input: UserInput!) { updateUser(userInfo: $input) { id name } }';
+            await API.graphql(graphqlOperation(mutation, { input: { name } }));
+            return true;
+        } catch (err) {
+            return false;
         }
-    }
-    `;
+    };
+
     return (
         <Connect query={graphqlOperation(query)}>
             {(response: IUserBlockQueryResponse) => {
                 if (response.loading) {
                     return (<UserBlock loading={response.loading}/>);
                 } else if (response.data && response.data.me) {
-                    return (<UserBlock name={response.data.me.name}/>);
+                    return (<UserBlock key={response.data.me.id} name={response.data.me.name} updateName={updateNameMutation}/>);
                 } else {
                     return (<UserBlock errors={response.errors}/>);
                 }
